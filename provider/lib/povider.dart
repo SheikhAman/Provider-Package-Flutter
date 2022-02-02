71d10fe0e5a6748e1d8d833f712e4de22b60c029
SheikhAman <amansk664@gmail.com>
2022-02-02 21:30:50 +0600

    using provider

--- provider/lib/povider.dart
+++ provider/lib/povider.dart
@@ -1,55 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final String data = 'Top Secret Data';
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(data),
+    return ChangeNotifierProvider<Data>(
+      create: (context) => Data(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: MyText(),
+          ),
+          body: Level1(),
         ),
-        body: Level1(data),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
-  final String data;
-  Level1(this.data);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(data),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  final String data;
-  Level2(this.data);
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(),
-        Level3(data),
+        MyTextField(),
+        Level3(),
       ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String data;
-  Level3(this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyText extends StatelessWidget {
+  const MyText({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(data);
+    return Text(Provider.of<Data>(context).data);
   }
+}
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'Top Secret Data';
 }
